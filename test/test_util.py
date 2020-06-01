ccc4dc04cdf72bd6bb9c0f8e9dab1da661f1b36e
harshita <harshita.1804@gmail.com>
2020-06-01 16:56:32 -0700

    generate_report.py

--- test/test_util.py
+++ test/test_util.py
@@ -3,6 +3,8 @@ import numpy as np
 import os
 import sys
 sys.path.append(os.getcwd() + '/..')
+os.chdir('..')
+import util
 
 
 @pytest.fixture(autouse=True)
@@ -17,10 +19,7 @@ def test_util():
     '''
     Tests util.py which has a dynamic_predict() function which is used to obtain predictions in a dynamic manner. 
     '''
-    os.chdir('..')
-    import util
 
-    
     p = util.dynamic_predict()
     assert type(p) == np.ndarray
     assert isinstance(p[0], (int,float))
